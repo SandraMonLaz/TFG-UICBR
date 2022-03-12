045eb0b94326a81b9126112d9cc83314e613e3dd
Sandra <samondra@ucm.es>
2022-03-12 13:22:36 +0100

    Pruebas de query-axios

--- REST_API/src/main/java/es/ucm/gdv/TFG/REST_API/GreetingController.java
+++ REST_API/src/main/java/es/ucm/gdv/TFG/REST_API/GreetingController.java
@@ -3,6 +3,7 @@ package es.ucm.gdv.TFG.REST_API;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class GreetingController {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}*/
 	
-	@GetMapping("/prueba")
+	@PostMapping("/prueba")
 	public Greeting prueba(@RequestBody Greeting input) {
 		//return new Greeting(1, "a");
 		return new Greeting(input.getId(), input.getContent(), input.getPrueba());
--- REST_API/src/main/resources/static/js/query.js
+++ REST_API/src/main/resources/static/js/query.js
@@ -11,11 +11,44 @@ function EnviarFormulario() {
 }
 
 function checkPlatfrom(){
-    if(document.getElementById("opcion-vida").checked){
-
+    var bodyFormData = {      
+        id: 1,
+        content: "Hello, World!",
+        prueba: {
+            holo: "high",
+            saludation: "holichiwinchis"
+        }
     }
+    //var bodyFormData = document.getElementById("myFormElement");
+    const location = new FormData();
+    location.set("id",1)
+    location.set("content", "bu")
+    
+    axios({
+        method: "post",
+        url: "/prueba",
+        data: bodyFormData,
+      })
+    .then(function (response) {
+          //handle success
+          console.log(response);
+    })
+    .catch(function (response) {
+          //handle error
+          console.log(response);
+    });
+    
 }
 
 function checkShooter(){
 
+}
+
+window.onload = function (){
+    console.log("Iniciando axios");
+    const instance = axios.create({
+        baseURL: 'localhost:8080',
+        timeout: 1000,
+        headers: {'X-Custom-Header': 'foobar'}
+      });
 }
